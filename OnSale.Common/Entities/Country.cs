921597b8d05d76ca23c3802c84e0a159f1f79d32
eduardopacompialopez <eduardopacompialopez@hotmail.com>
2021-10-26 17:39:44 -0500

    COMPLETO HASTA LA MITAD DEL VIDEO 2 DEL PROYECTO ONSALE

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        public string Name { get; set; }
+    }
+}
